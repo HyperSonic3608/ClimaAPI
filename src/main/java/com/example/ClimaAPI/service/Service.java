fa2011bd105dbaa6da4e77308a66dd09e9157840
Davi Sollar <davisollar@gmail.com>
2026-08-24 08:35:11 -0300

    Adicionado função de consulta ao tempo

--- src/main/java/com/example/ClimaAPI/service/Service.java
+++ src/main/java/com/example/ClimaAPI/service/Service.java
@@ -1,5 +1,22 @@
 package com.example.ClimaAPI.service;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
 public class Service {
 
+    private final String apiUrl = "https://api.open-meteo.com/v1/forecast?latitude=-19.9208&longitude=-43.9378&hourly=temperature_2m&timezone=America%2FSao_Paulo";
+
+    public String pegarTempoBH() {
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(apiUrl, String.class);
+
+        if (responseEntity.getStatusCode().is2xxSuccessful()) {
+            return responseEntity.getBody();
+        } else {
+            return "Falha ao obter dados meteorológicos de BH. Código: " + responseEntity.getStatusCode();
+        }
+
+    }
 }
